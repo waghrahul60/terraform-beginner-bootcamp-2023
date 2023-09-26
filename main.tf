4b92f72e220459c9f7308a7a3fb6350bfa7fddb5
Rahul Wagh <waghrahul60@gmail.com>
2023-09-26 20:21:48 +0000

    Creating S3 bucket

--- main.tf
+++ main.tf
@@ -1,8 +1,12 @@
 terraform {
   required_providers {
     random = {
-      source = "hashicorp/random"
+      source  = "hashicorp/random"
       version = "3.5.1"
+    }
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.17.0"
     }
   }
 }
@@ -11,9 +15,19 @@ provider "random" {
   # Configuration options
 }
 
+provider "aws" {
+  # Configuration options
+}
+
 resource "random_string" "bucket_name" {
-  length           = 16
-  special          = false
+  length  = 16
+  special = false
+  lower   = true
+  upper   = false
+}
+
+resource "aws_s3_bucket" "example" {
+  bucket = random_string.bucket_name.id
 }
 
 output "random_bucket_name_id" {
